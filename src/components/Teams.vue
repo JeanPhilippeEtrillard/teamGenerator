22a29528e2afa65dff4dfae3ac958828b4ff514f
Jean-Philippe ETRILLARD <jphetr@gmail.com>
2020-11-04 13:25:38 +0100

    add equals repartition of dps

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -5,27 +5,27 @@
             <v-btn outlined color="blue-grey" large>RETOUR</v-btn>
         </router-link>
         <v-divider class="mt-5"></v-divider>
-            <v-navigation-drawer permanent absolute>
-                <v-list dense nav>
-                    <v-list-item v-if="players.length>0">
-                        <v-btn color="info" @click="generateTeams" small>Generate Teams</v-btn>
-                        <v-btn color="error" @click="clear" small> Clear</v-btn>
-                    </v-list-item>
-                    <v-list-item v-if="players.length===0">
-                        <v-btn class="playerList" disabled>No Data Found</v-btn>
-                    </v-list-item>
-                    <v-list-item v-for="(player,i) in players" :key="i">
-                        <PlayerVue class="playerList" v-bind:player="player" v-bind:index="i"/>
-                    </v-list-item>
-                </v-list>
-            </v-navigation-drawer>
-            <v-main class="teamsPanel">
-                <v-row>
-                    <v-col class="playerCard" v-for="(team,i) in teams" :key="i" sm="2">
-                        <TeamVue v-bind:team="team" v-bind:index="i"/>
-                    </v-col>
-                </v-row>
-            </v-main>
+        <v-navigation-drawer permanent absolute>
+            <v-list dense nav>
+                <v-list-item v-if="players.length>0">
+                    <v-btn color="info" @click="generateTeams" small>Generate Teams</v-btn>
+                    <v-btn color="error" @click="clear" small> Clear</v-btn>
+                </v-list-item>
+                <v-list-item v-if="players.length===0">
+                    <v-btn class="playerList" disabled>No Data Found</v-btn>
+                </v-list-item>
+                <v-list-item v-for="(player,i) in players" :key="i">
+                    <PlayerVue class="playerList" v-bind:player="player" v-bind:index="i"/>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-main class="teamsPanel">
+            <v-row>
+                <v-col class="playerCard" v-for="(team,i) in teams" :key="i" sm="2">
+                    <TeamVue v-bind:team="team" v-bind:index="i"/>
+                </v-col>
+            </v-row>
+        </v-main>
 
     </div>
 </template>
@@ -45,8 +45,7 @@
             }
         },
         data: () => ({
-            teams: [
-            ]
+            teams: []
         }),
         methods: {
             generateTeams() {
@@ -56,30 +55,34 @@
 
 
                 var nbTeam = Math.max(healerList.length, tankList.length, dpsList.length % 3)
-                for(let nb=0; nb<nbTeam; nb++  ){
-                    var tankIdx = Math.floor(Math.random()*(tankList.length))
-                    var healIdx = Math.floor(Math.random()*(healerList.length))
-                    var dpsListTmp =[];
+                var sliceDps = dpsList.length / nbTeam;
+                for (let nb = 0; nb < nbTeam; nb++) {
+                    var tankIdx = Math.floor(Math.random() * (tankList.length))
+                    var healIdx = Math.floor(Math.random() * (healerList.length))
+                    var dpsListTmp = [];
 
-                    for(let i=0; i<3 && i <= dpsList.length; i++){
-                        var dpsIdx = Math.floor(Math.random()*(dpsList.length))
+                    console.log(sliceDps)
+                    for (let i = 0; ((!Number.isInteger(sliceDps) && i < 3) || (Number.isInteger(sliceDps) && i < sliceDps && i < 3)) && i <= dpsList.length;
+                         i++
+                    ) {
+                        var dpsIdx = Math.floor(Math.random() * (dpsList.length))
                         dpsListTmp.push(dpsList[dpsIdx]);
-                        dpsList.splice(dpsIdx,1)
+                        dpsList.splice(dpsIdx, 1)
                     }
 
                     var team = {
                         tank: tankList[tankIdx],
                         heal: healerList[healIdx],
-                        dps:dpsListTmp
+                        dps: dpsListTmp
                     }
                     this.teams.push(team)
-                    tankList.splice(tankIdx,1)
-                    healerList.splice(healIdx,1)
+                    tankList.splice(tankIdx, 1)
+                    healerList.splice(healIdx, 1)
                 }
                 this.$forceUpdate();
             },
-            clear(){
-                this.teams.splice(0,this.teams.length);
+            clear() {
+                this.teams.splice(0, this.teams.length);
                 this.$forceUpdate();
             }
         }
